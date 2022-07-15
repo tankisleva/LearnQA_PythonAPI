58e46a3f0b2e0d6b9355bd3e3fdf8695095369ff
Oleg Malyshev <malyshev@jivosite.com>
2022-07-15 17:51:47 +0300

    get request

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import requests
 
-print("Hello from Oleg")
+response = requests.get("https://playground.learnqa.ru/api/get_text")
 
-
-# response = requests.get("https://playground.learnqa.ru/api/hello")
-# print(response.text)
+print(response.text)
