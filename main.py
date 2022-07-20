840de3383898354096bab22d6e421dd94db42c06
Oleg Malyshev <malyshev@jivosite.com>
2022-07-20 22:32:34 +0300

    parsing json

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import requests
+import json
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
+json_text = {
+  "messages": [
+    {"message": "This is the first message", "timestamp": "2021-06-04 16:40:53"},
+    {"message": "And this is a second message", "timestamp": "2021-06-04 16:41:01"}
+  ]
+}
 
-print(response.text)
+json_dump = json.dumps(json_text)
+json_object = json.loads(json_dump)
+
+print(json_object["messages"][1])
+
