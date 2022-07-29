d3a42dee03215ec2ece85e0520d0cbec490ee533
Oleg Malyshev <malyshev@jivosite.com>
2022-07-29 12:39:45 +0300

    test_short_phrase

--- short_phrase.py
+++ short_phrase.py
@@ -2,7 +2,6 @@ class TestExample:
 
     def test_short_phrase(self):
         phrase = input("Set a phrase less than 15 symbols:")
-        print(len(phrase))
         expected_len = 15
         actual_len = len(phrase)
-        assert actual_len < expected_len, f"{actual_len} is now more then {expected_len}"
+        assert actual_len < expected_len, f"{actual_len} is now more than {expected_len}"
