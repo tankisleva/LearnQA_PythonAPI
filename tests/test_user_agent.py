f1603444061374adbc6637ba3c4603d07a3fe65d
Oleg Malyshev <malyshev@jivosite.com>
2022-08-06 17:41:10 +0300

    test framework

---
+++ tests/test_user_agent.py
@@ -0,0 +1,61 @@
+import json
+
+import pytest
+import requests
+
+
+class TestUserAgents:
+    data = [
+        (
+            "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+            "Mobile",
+            "No",
+            "Android"
+        ),
+        (
+            "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1",
+            "Mobile",
+            "Chrome",
+            "iOS"
+        ),
+        (
+            "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+            "Googlebot",
+            "Unknown",
+            "Unknown"
+        ),
+        (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
+            "Googlebot",
+            "Chrome",
+            "No"
+        ),
+        (
+            "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
+            "Mobile",
+            "No",
+            "iPhone"
+        )
+    ]
+
+    @pytest.mark.parametrize('agent, platform, browser, device', data)
+    def test_check_agent(self, agent, platform, browser, device):
+        url = "https://playground.learnqa.ru/ajax/api/user_agent_check"
+        data = {"user-agent": agent}
+        print(agent)
+
+        response = requests.get(url, headers=data)
+        obj = json.loads(response.text)
+
+        assert obj.get("browser") == browser
+        assert obj.get("device") == device
+        assert obj.get("platform") == platform
+
+
+
+
+
+
+
+
+
---
+++ tests/test_user_edit.py
@@ -0,0 +1,51 @@
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+from lib.my_requests import MyRequests
+
+class TestUserEdit(BaseCase):
+    def test_edit_just_created_user(self):
+
+        # REGISTER
+        registration_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user", data=registration_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = registration_data['email']
+        first_name = registration_data['firstName']
+        password = registration_data['password']
+        user_id = self.get_json_value(response1, "id")
+
+        # LOGIN
+        login_fata = {
+            'email': email,
+            'password': password
+        }
+
+        response2 = MyRequests.post("/user/login", data=login_fata)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # EDIT
+        new_name = "Changed_name"
+
+        response3 = MyRequests.put(f"/user/{user_id}",
+                                 headers={"x-csrf-token": token},
+                                 cookies={"auth_sid": auth_sid},
+                                 data={"firstName": new_name}
+                                 )
+        Assertions.assert_code_status(response3, 200)
+
+        # GET
+        response4 = MyRequests.get(f"/user/{user_id}",
+                                 headers={"x-csrf-token": token},
+                                 cookies={"auth_sid": auth_sid}
+                                 )
+
+        Assertions.assert_json_value_by_name(response4, "firstName", new_name, "Wrong name of user after edit")
+
+
+
+
